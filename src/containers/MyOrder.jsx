399cc0e13f9cbba510e4d710846f655104880337
Hernán D. Mercado <hernandmf@gmail.com>
2021-10-19 14:20:51 -0500

    Sum total price function

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -6,7 +6,11 @@ import flechita from "@icons/flechita.svg";
 
 const MyOrder = () => {
   const { state } = React.useContext(AppContext);
-
+  const sumTotal = () => {
+    const reducer = (acum, currentValue) => acum + currentValue.price;
+    const sum = state.cart.reduce(reducer, 0);
+    return sum;
+  };
   return (
     <aside className="MyOrder">
       <div className="title-container">
@@ -21,7 +25,7 @@ const MyOrder = () => {
           <p>
             <span>Total</span>
           </p>
-          <p>$560.00</p>
+          <p>${sumTotal()}</p>
         </div>
         <button className="primary-button">Checkout</button>
       </div>
