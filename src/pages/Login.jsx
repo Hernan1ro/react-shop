e548bccab8863005592ca86e7200582bfc4ade3d
Hernán D. Mercado <hernandmf@gmail.com>
2021-10-19 10:16:16 -0500

    use Ref for form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,17 +3,30 @@ import "../styles/Login.scss";
 import logoYardSale from "@logos/logo_yard_sale.svg";
 
 const Login = () => {
+  const form = React.useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(form.current);
+    const usuario = {
+      name: formData.get("email"),
+      email: formData.get("password"),
+    };
+    console.log(usuario);
+  };
+
   return (
     <div className="Login">
       <div className="Login-container">
         <img src={logoYardSale} alt="logo" className="logo" />
-        <form action="/" className="form">
+        <form action="/" className="form" ref={form}>
           <label htmlFor="email" className="label">
             Email address
           </label>
           <input
             type="text"
             id="email"
+            name="email"
             placeholder="platzi@example.cm"
             className="input input-email"
           />
@@ -23,10 +36,12 @@ const Login = () => {
           <input
             type="password"
             id="password"
+            name="password"
             placeholder="*********"
             className="input input-password"
           />
           <input
+            onClick={handleSubmit}
             type="submit"
             value="Log in"
             className="primary-button login-button"
